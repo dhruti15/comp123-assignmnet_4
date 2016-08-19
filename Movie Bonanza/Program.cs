737efc731ae375ec3bf6af5bc9bb91af218bf2ed
Dhrutiben Patel <dpate483@my.centennialcollege.ca>
2016-08-19 20:37:29 +0100

    initial commit

--- Movie Bonanza/Program.cs
+++ Movie Bonanza/Program.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+/// <summary>
+/// 
+/// </summary>
 namespace Movie_Bonanza
 {
     static class Program
